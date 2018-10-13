3cecb0099f5947dff79c217d3c8ccd97e5222711
Šimo Matijević <simo.matijevic@gmail.com>
2018-10-14 00:44:37 +0200

    Update baza.php

--- baza.php
+++ baza.php
@@ -10,23 +10,16 @@
     if(!$poveznica ){
       die('Spajanje neuspjelo! ' . mysqli_error());
     }
-    	
-
     mysqli_set_charset($poveznica,"utf8");
     return $poveznica;
   }
   function izvrsiUpit($poveznica,$upit){
     mysqli_set_charset($poveznica,"utf-8");
-    
     $rezultat=mysqli_query($poveznica,$upit);
-    
-
-    if(mysqli_error($poveznica)!=="")echo '<h1 id="greska">GREŠKA: Problem sa upitom </h1>';
-    
-
-    
-
-    return $rezultat;
+    if(mysqli_error($poveznica)!==""){
+      echo '<h1 id="greska">GREŠKA: Problem sa upitom </h1>'
+     };
+   return $rezultat;
   }
 
 ?>
